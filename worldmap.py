9c769b88ca5380b53515d5c04dd5a1bd3c6a67d3
El Saico <bomarques@inf.ufrgs.br>
2011-02-02 20:17:26 +0000

    This is not Java: unificando classes de mapa num só arquivo

--- worldmap.py
+++ worldmap.py
@@ -1,8 +1,60 @@
+# coding: utf-8
 from random import shuffle
 from itertools import cycle, chain
 from json import loads, dumps
-from basemap import BaseMap
+
 from territory import Territory
+
+class BaseMap:
+	def __init__(self, doc):
+		self._borders = set(map(tuple, doc["borders"]))
+		self._bonus = dict((x["name"], x["bonus"]) for x in doc["continents"])
+		self.colors = tuple(doc["colors"])
+		self.shapes = tuple(doc["shapes"])
+		
+	def __str__(self):
+		return "\n".join("{0} - {1.owner}, {1.armySize}".format(n, t)
+							for n, t in self._countries.items())
+	
+	def neighbors(self, t1, t2):
+		return (t1, t2) in self._borders or (t2, t1) in self._borders
+		
+	def reinforce(self, target, army):
+		self._countries[target].reinforce(army)
+		
+	def country(self, t):
+		return self._countries[t]
+	
+	def countries(self):
+		return self._countries.keys()
+	
+	def owner(self, t):
+		return self._countries[t].owner
+	
+	def army(self, t):
+		return self._countries[t].armySize
+	
+	def continent(self, c):
+		return self._continents[c]
+	
+	def continents(self):
+		return self._continents.keys()
+	
+	def continentBonus(self, c):
+		return self._bonus[c]
+
+class ClientMap(BaseMap):
+	def __init__(self, json, players):
+		doc = loads(json)
+		BaseMap.__init__(self, doc)
+		self._players = players
+		self._continents = {}
+		self._countries = {}
+		for ctn in doc["continents"]:
+			self._continents[ctn["name"]] = []
+			for cty in ctn["countries"]:
+				self._continents[ctn["name"]].append(cty["name"])
+				self._countries[cty["name"]] = Territory(cty["army"], cty["owner"])
 
 class ServerMap(BaseMap):
 	def __init__(self, json, players):
