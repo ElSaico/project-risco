02f8072d6523f41b24551c76935f1bd7bd69e98d
El Saico <bomarques@inf.ufrgs.br>
2011-02-18 01:09:55 +0000

    Corrigindo chaves estrangeiras

--- game/models.py
+++ game/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Territory(models.Model):
 	name = models.CharField(max_length=30)
-	continent = models.ForeignKey(Continent)
-	board = models.ForeignKey(Board)
+	continent = models.ForeignKey('Continent')
+	board = models.ForeignKey('Board')
 	borders = models.ManyToManyField('self')
 
 class Continent(models.Model):
@@ -17,14 +18,14 @@ class Card(models.Model):
 
 class GameTerritory(models.Model):
 	territory = models.ForeignKey(Territory)
-	game = models.ForeignKey(Game, related_name="territory_list")
-	owner = models.ForeignKey(Player, related_name="territory_list")
+	game = models.ForeignKey('Game', related_name="territory_list")
+	owner = models.ForeignKey('Player', related_name="territory_list")
 	army = models.IntegerField(default=1)
 	relocated_army = models.IntegerField(default=0)
 
 class Player(models.Model):
 	user = models.ForeignKey(User)
-	game = models.ForeignKey(Game)
+	game = models.ForeignKey('Game')
 	cards = models.ManyToManyField(Card)
 	color = models.CharField(max_length=10)
 	playing = models.BooleanField(default=True)
@@ -37,14 +38,14 @@ class Player(models.Model):
 class Game(models.Model):
 	running = models.BooleanField(default=False)
 	name = models.CharField(unique=True, max_length=30)
-	password = models.CharField(min_length=4)
-	board = models.ForeignKey(Board)
+	password = models.CharField()
+	board = models.ForeignKey('Board')
 	turn = models.IntegerField(default=1)
 	turn_player = models.IntegerField(default=0) # an index, to facilitate iteration
 	global_trade = models.BooleanField()
 	step = models.CharField(max_length=10)
 
 class Board(models.Model):
-	name = models.StringField(unique=True)
+	name = models.CharField(unique=True)
 	early_trades = models.CommaSeparatedIntegerField(max_length=30)
 	late_trades = models.IntegerField()
