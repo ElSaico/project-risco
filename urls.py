13613994a7d036b7071728a2421efcc984c1e537
Bruno Marques <bomarques@inf.ufrgs.br>
2011-12-27 00:40:29 +0000

    Desativando admin + requisitos do virtualenv

--- urls.py
+++ urls.py
@@ -2,18 +2,9 @@ from django.conf.urls.defaults import *
 from jsonrpc import jsonrpc_site
 
 import game.remote
-from game.models import Board, Continent, Territory, Card
-
-# Uncomment the next two lines to enable the admin:
-from django.contrib import admin
-admin.autodiscover()
-for model in (Board, Continent, Territory, Card):
-	admin.site.register(model)
 
 urlpatterns = patterns('',
     url(r'^call/browse/', 'jsonrpc.views.browse', name='jsonrpc_browser'),
     url(r'^call/', jsonrpc_site.dispatch, name='jsonrpc_mountpoint'),
     url(r'^auth/', include('django_openid_auth.urls')),
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    url(r'^admin/(.*)', admin.site.root),
 )
