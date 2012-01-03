8c8090989ff74107fc290c228be2ae27d0358374
Bruno Marques <bomarques@inf.ufrgs.br>
2012-01-03 15:55:20 +0000

    Suporte a chamadas RPC GET

--- projectrisco/urls.py
+++ projectrisco/urls.py
@@ -5,6 +5,7 @@ import game.remote
 
 urlpatterns = patterns('',
     url(r'^call/browse/', 'jsonrpc.views.browse', name='jsonrpc_browser'),
+    url(r'^call/(?P<method>[a-zA-Z0-9.-_]+)$', jsonrpc_site.dispatch),
     url(r'^call/', jsonrpc_site.dispatch, name='jsonrpc_mountpoint'),
     url(r'^auth/', include('social_auth.urls')),
     url(r'^auth/logout$', 'game.views.logout', name='logout'),
