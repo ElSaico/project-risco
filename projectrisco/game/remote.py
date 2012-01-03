a307b650d1a7756ffcbebb217d35f28d40a6605c
Bruno Marques <bomarques@inf.ufrgs.br>
2012-01-03 12:25:23 +0000

    Limpeza na API
    - Backends de autenticação restritos a Google, Yahoo, Facebook e Twitter
    - Namespace "pyWar" removido
    - "user.status" de volta
    - Unificação das chamadas "games" e "user.games" em "game.list"
    - Corrigindo RPCs que não acompanharam mudanças no modelo

--- projectrisco/game/remote.py
+++ projectrisco/game/remote.py
@@ -1,3 +1,4 @@
+from django.core.urlresolvers import reverse
 from jsonrpc import jsonrpc_method
 from jsonrpc.exceptions import Error
 
@@ -9,21 +10,21 @@ def _get_obj(name, cls, error_msg):
 	except cls.DoesNotExist:
 		raise Error, error_msg
 
-@jsonrpc_method('pyWar.games')
-def list_games(request, filters={}):
-	games = Game.objects.filter(**filters)
-	return [ {"name": game.name,
-	         "board": str(game.board),
-	   "num_players": game.player_set.count(),
-	  "has_password": bool(game.password),
-	  "global_trade": game.global_trade,
-	    "objectives": game.objectives,
-	       "running": game.running,
-	         } for game in games ]
+@jsonrpc_method('user.status')
+def user_data(request):
+	response = {'logged': request.user.is_authenticated()}
+	if not response['logged']:
+		response['login_url'] = {backend.title(): reverse('socialauth_login', args=[backend])
+		                         for backend in ('twitter', 'facebook', 'google', 'yahoo')}
+	else:
+		response['logout_url'] = reverse('logout')
+	return response
 
-@jsonrpc_method('pyWar.user.games', authenticated=True)
-def user_games(request):
-	games = Game.objects.filter(players__user=request.user)
+@jsonrpc_method('game.list')
+def list_games(request, with_user=False, filters={}):
+	games = Game.objects.filter(**filters)
+	if with_user:
+		games = games.filter(players__user=request.user)
 	return [ {"name": game.name,
 	         "board": str(game.board),
 	   "num_players": game.players.count(),
@@ -33,7 +34,7 @@ def user_games(request):
 	       "running": game.running,
 	         } for game in games ]
 	
-@jsonrpc_method('pyWar.game.create', authenticated=True)
+@jsonrpc_method('game.create', authenticated=True)
 def create_game(request, game_name, game_password, board_name, objectives, global_trade, player_color):
 	if objectives:
 		raise Error, "Objectives not implemented yet"
@@ -42,32 +43,32 @@ def create_game(request, game_name, game_password, board_name, objectives, globa
 	                           objectives=objectives, global_trade=global_trade)
 	Player.objects.create(user=request.user, game=game, color=player_color)
 
-@jsonrpc_method('pyWar.game.join', authenticated=True)
+@jsonrpc_method('game.join', authenticated=True)
 def join_game(request, color, game_name, game_password):
 	game = _get_obj(game_name, Game, "Game doesn't exist")
 	if game.running:
 		raise Error, "Game is already running"
-	player = game.player_set.filter(user=request.user)
+	player = game.players.filter(user=request.user)
 	if player.exists():
 		raise Error, "Player already in game"
-	if game.player_set.count() >= game.board.max_players:
+	if game.players.count() >= game.board.max_players:
 		raise Error, "Limit of players reached"
-	game_color = game.player_set.filter(color=color)
+	game_color = game.players.filter(color=color)
 	if game_color.exists():
 		raise Error, "Color already in use"
 	if game.password and game_password != game.password:
 		raise Error, "Invalid password"
 	Player.objects.create(user=request.user, game=game, color=color)
 
-@jsonrpc_method('pyWar.game.start', authenticated=True)
+@jsonrpc_method('game.start', authenticated=True)
 def start_game(request, game_name):
 	game = _get_obj(game_name, Game, "Game doesn't exist")
 	if game.running:
 		raise Error, "Game is already running"
-	player = game.player_set.filter(user=request.user)
+	player = game.players.filter(user=request.user)
 	if not player.exists():
 		raise Error, "Player not in game"
-	if game.player_set.count() < game.board.min_players:
+	if game.players.count() < game.board.min_players:
 		raise Error, "Too few players"
 	try:
 		game.start()
--- projectrisco/settings.py
+++ projectrisco/settings.py
@@ -105,16 +105,14 @@ AUTHENTICATION_BACKENDS = (
     #'social_auth.backends.google.GoogleOAuth2Backend',
     'social_auth.backends.google.GoogleBackend',
     'social_auth.backends.yahoo.YahooBackend',
-    'social_auth.backends.contrib.linkedin.LinkedinBackend',
-    'social_auth.backends.contrib.livejournal.LiveJournalBackend',
+    #'social_auth.backends.contrib.linkedin.LinkedinBackend',
+    #'social_auth.backends.contrib.livejournal.LiveJournalBackend',
     #'social_auth.backends.contrib.orkut.OrkutBackend',
-    'social_auth.backends.contrib.foursquare.FoursquareBackend',
-    'social_auth.backends.contrib.github.GithubBackend',
-    'social_auth.backends.OpenIDBackend',
+    #'social_auth.backends.contrib.foursquare.FoursquareBackend',
+    #'social_auth.backends.contrib.github.GithubBackend',
+    #'social_auth.backends.OpenIDBackend',
     'django.contrib.auth.backends.ModelBackend',
 )
-
-SOCIAL_AUTH_ASSOCIATE_BY_MAIL = True
 
 LOGIN_URL = '/auth/login/'
 LOGIN_REDIRECT_URL = '/'
