44d48eac0f09d02758ddb576a4056e3f01ee48f9
Bruno Marques <bomarques@inf.ufrgs.br>
2012-01-06 02:54:47 +0000

    Suporte a chamadas externas para a API (incompleto)

--- projectrisco/urls.py
+++ projectrisco/urls.py
@@ -1,12 +1,9 @@
 from django.conf.urls.defaults import *
-from jsonrpc import jsonrpc_site
-
-import game.remote
 
 urlpatterns = patterns('',
     url(r'^call/browse/', 'jsonrpc.views.browse', name='jsonrpc_browser'),
-    url(r'^call/(?P<method>[a-zA-Z0-9.-_]+)$', jsonrpc_site.dispatch),
-    url(r'^call/', jsonrpc_site.dispatch, name='jsonrpc_mountpoint'),
+    url(r'^call/(?P<method>[a-zA-Z0-9.-_]+)$', 'game.views.xhr_dispatch'),
+    url(r'^call/', 'game.views.xhr_dispatch', name='jsonrpc_mountpoint'),
     url(r'^auth/', include('social_auth.urls')),
     url(r'^auth/logout$', 'game.views.logout', name='logout'),
 )
