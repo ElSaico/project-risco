33ae47a10b79575fe3fceece9c171b4277ee7d88
Bruno Marques <bomarques@inf.ufrgs.br>
2012-01-14 00:32:31 +0000

    Começando migração para Tornado + MongoDB

--- projectrisco/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('',
-    url(r'^call/browse/', 'jsonrpc.views.browse', name='jsonrpc_browser'),
-    url(r'^call/(?P<method>[a-zA-Z0-9.-_]+)$', 'game.views.xhr_dispatch'),
-    url(r'^call/', 'game.views.xhr_dispatch', name='jsonrpc_mountpoint'),
-    url(r'^auth/', include('social_auth.urls')),
-    url(r'^auth/logout$', 'game.views.logout', name='logout'),
-)
