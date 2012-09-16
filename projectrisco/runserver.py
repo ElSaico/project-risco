fe6f1ac7271eaed33bdcde71c3930fa2dea195c7
Bruno Marques <bomarques@inf.ufrgs.br>
2012-09-16 17:58:44 +0000

    Descrição de tabuleiro (com alguns campos, apenas)

--- projectrisco/runserver.py
+++ projectrisco/runserver.py
@@ -1,5 +1,5 @@
 from tornado.ioloop import IOLoop
-from tornado.web import Application
+from tornado.web import Application, url
 
 import auth
 import games
@@ -9,10 +9,10 @@ import users
 from common import options
 
 urls = [
-	#(r"/", webclient.IndexHandler),
-	(r"/boards", boards.HTMLHandler),
-	#(r"/boards/([0-9a-f]+)", boards.HTMLHandler),
-	(r"/login/google", auth.GoogleHandler),
+	#url(r"/", webclient.IndexHandler, name='index'),
+	url(r"/boards", boards.HTMLHandler, name='boards'),
+	url(r"/boards/([0-9a-f]+)", boards.HTMLHandler, name='board'),
+	url(r"/login/google", auth.GoogleHandler, name='google'),
 	(r"/resource/user", users.RESTHandler),
 	(r"/resource/board", boards.RESTHandler),
 	(r"/resource/board/([0-9a-f]+)", boards.RESTHandler),
