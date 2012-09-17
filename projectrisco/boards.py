aba06e73ba3afa0133093ad9c689cb5426db9c34
Bruno Marques <bomarques@inf.ufrgs.br>
2012-09-17 21:54:15 +0000

    Breadcrumbs (isso é realmente necessário?)

--- projectrisco/boards.py
+++ projectrisco/boards.py
@@ -55,12 +55,14 @@ class HTMLHandler(common.RiscoHandler):
 		except:
 			raise HTTPError(404)
 
+		breadcrumbs = {'Home': '/', 'Mapas': self.reverse_url('boards')}
 		if board_id:
+			breadcrumbs[data['name']] = self.reverse_url('board', data['id'])
 			template = self.templates.get_template('board.html')
-			self.write(template.render(board=data))
+			self.write(template.render(board=data, breadcrumbs=breadcrumbs))
 		else:
 			template = self.templates.get_template('boards.html')
-			self.write(template.render(boards=data['boards']))
+			self.write(template.render(boards=data['boards'], breadcrumbs=breadcrumbs))
 
 @Vows.batch
 class BoardTest(common.RiscoVows):
